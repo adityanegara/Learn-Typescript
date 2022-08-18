d4f502b0ac72b830c309ef65df3a3d7182836976
Aditya Negara <aditnegarabisnis@gmail.com>
2022-08-18 20:58:05 +0700

    Finish resistor color excercise

--- 9. Excercise/src/4.LeapYear.ts
+++ 9. Excercise/src/4.LeapYear.ts
@@ -1,4 +1,4 @@
-const isLeapYear = (year: number): boolean => {
+export const isLeapYear = (year: number): boolean => {
     if (year % 4 === 0) {
         if (year % 100 === 0) {
             if (year % 400 === 0) {
@@ -11,12 +11,3 @@ const isLeapYear = (year: number): boolean => {
     return false;
 }
 
-console.log(isLeapYear(2015));
-console.log(isLeapYear(1970));
-console.log(isLeapYear(1996));
-console.log(isLeapYear(1960));
-console.log(isLeapYear(2100));
-console.log(isLeapYear(1900));
-console.log(isLeapYear(2000));
-console.log(isLeapYear(2400));
-console.log(isLeapYear(1800));
