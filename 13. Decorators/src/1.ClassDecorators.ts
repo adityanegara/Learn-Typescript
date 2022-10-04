93f9756983ee4d11dd2b64d43b64552f0cd6a504
Aditya Negara <Aditya_N801@mitrais.com>
2022-10-04 07:42:08 +0700

    testing

--- 13. Decorators/src/1.ClassDecorators.ts
+++ 13. Decorators/src/1.ClassDecorators.ts
@@ -1,5 +1,6 @@
 function Component(constructor: Function) {
     console.log('Component decorator called');
+    console.log('testing');
     constructor.prototype.uniqueId = Date.now();
     constructor.prototype.insertInDOM = () => {
         console.log('Inserting component in the DOM')
