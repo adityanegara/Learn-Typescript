8f2778a0d5f339d9a573adc3d66cbef883b5be91
Aditya Negara <Aditya_N801@mitrais.com>
2022-10-04 09:26:57 +0700

    Learn Default Export

---
+++ 14. Modules/src/2.Default Export/index.ts
@@ -0,0 +1,4 @@
+import Triangle, {Circle} from './shape';
+
+const triangle = new Triangle(3,4,5);
+const circle = new Circle(7);
---
+++ 14. Modules/src/2.Default Export/shape.ts
@@ -0,0 +1,14 @@
+export default class rectangle{
+    constructor(
+        private _sideA: number,
+        private _sideB: number,
+        private _sideC: number){
+
+        }
+}
+
+export class Circle{
+    constructor(private _radius: number){
+        
+    }
+}
