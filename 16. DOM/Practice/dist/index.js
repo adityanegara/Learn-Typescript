b32afea015fb8e50b3f9601f64a82de6ece8ca92
Aditya Negara <Aditya_N801@mitrais.com>
2022-10-19 11:37:53 +0700

    Finish adding validation to typescript DOM practice

--- 16. DOM/Practice/dist/index.js
+++ 16. DOM/Practice/dist/index.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("Hello World");
+const noteInput = document.getElementById('note-form__input');
+const submitNoteButton = document.querySelector('.note-form');
+const warningText = document.querySelector('.warning-text');
+const isStringEmpty = (text) => {
+    return (text == null || text == "");
+};
+const showElement = (element) => {
+    element.classList.remove('display-none');
+    element.classList.add('display-visible');
+};
+const hideElement = (element) => {
+    element.classList.add('display-none');
+    element.classList.remove('display-visible');
+};
+submitNoteButton.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (isStringEmpty(noteInput.value)) {
+        showElement(warningText);
+    }
+    else {
+        hideElement(warningText);
+    }
+});
--- 16. DOM/Practice/src/index.ts
+++ 16. DOM/Practice/src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const noteInput = document.getElementById('note-form__input') as HTMLInputElement;
+const submitNoteButton = document.querySelector('.note-form') as HTMLFormElement;
+const warningText = document.querySelector('.warning-text') as HTMLElement;
+
+const isStringEmpty = (text: string):boolean =>{
+    return (text == null || text == "");
+}
+
+const showElement = (element: HTMLElement):void =>{
+    element.classList.remove('display-none');
+    element.classList.add('display-visible');
+}
+
+const hideElement = (element: HTMLElement):void =>{
+    element.classList.add('display-none');
+    element.classList.remove('display-visible');
+}
+
+
+submitNoteButton.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    if(isStringEmpty(noteInput.value)){
+        showElement(warningText);
+    }else{
+        hideElement(warningText);
+    }
+})
